431eee80168ad75ae454bf0546b831600489a6bf
Sandpitturtleee <karolmazur@onet.eu>
2021-08-19 00:35:41 +0200

    Test Ascii

--- Test.cpp
+++ Test.cpp
@@ -7,5 +7,14 @@ int main()
     int a = 'A';
     cout<<"tekst"<<endl;
     cout<<a<<endl;
+    cout<<char(65)<<endl;
+    char tekst[5];
+    cout<<"Podaj tekst: ";
+    cin>>tekst;
+    for(int i=0;i<5;i++)
+    {
+        cout<<tekst[i]<<" ";
+        cout<<int(tekst[i])<<" ";
+    }
     return 0;
 }
