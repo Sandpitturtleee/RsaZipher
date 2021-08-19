2a851c3ac709e1cc2f8ea12ae78d1954def2c558
Sandpitturtleee <karolmazur@onet.eu>
2021-08-19 12:40:15 +0200

    Creating Ascii char array

--- Test.cpp
+++ Test.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 int main()
 {
-    int a = 'A';
-    cout<<"tekst"<<endl;
-    cout<<a<<endl;
-    cout<<char(65)<<endl;
-    char tekst[5];
+    int lenght = 0;
+    char tekst[13];    
+    int tekstAscii[33];
     cout<<"Podaj tekst: ";
     cin>>tekst;
-    for(int i=0;i<5;i++)
+    cout<<strlen(tekst)<<endl;
+    for(int i=0;i<13;i++)
     {
-        cout<<tekst[i]<<" ";
-        cout<<int(tekst[i])<<" ";
+        
+        tekstAscii[i]=int(tekst[i]);
+        cout<<tekstAscii[i];
     }
+    cout<<endl;
     return 0;
+    //33
 }
